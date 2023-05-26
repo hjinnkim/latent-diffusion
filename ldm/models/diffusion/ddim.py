7ce0c5e95eee53504dc1079298d1bc6734e3efc4
hjinnkim <khj12203@gmail.com>
2023-05-27 08:28:01 +0900

    Edit for ldm

--- ldm/models/diffusion/ddim.py
+++ ldm/models/diffusion/ddim.py
@@ -79,11 +79,11 @@ class DDIMSampler(object):
                ):
         if conditioning is not None:
             if isinstance(conditioning, dict):
-                cbs = conditioning[list(conditioning.keys())[0]].shape[0]
+                cbs = conditioning[list(conditioning.keys())[0]][0].shape[0]
                 if cbs != batch_size:
                     print(f"Warning: Got {cbs} conditionings but batch-size is {batch_size}")
             else:
-                if conditioning.shape[0] != batch_size:
+                if conditioning[0].shape[0] != batch_size:
                     print(f"Warning: Got {conditioning.shape[0]} conditionings but batch-size is {batch_size}")
 
         self.make_schedule(ddim_num_steps=S, ddim_eta=eta, verbose=verbose)
